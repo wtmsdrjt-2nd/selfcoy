1c642cee56195d8b77aeeaa9863b61b9b9418d4d
wtmsdrjt-2nd <141470842+wtmsdrjt-2nd@users.noreply.github.com>
2024-06-22 11:54:27 +0700

    Update client.js

--- client.js
+++ client.js
@@ -141,7 +141,7 @@ client.on('presence.update', update => {
    } else {}
 })
 
-client.on('group.add', async ctx => {
+/*client.on('group.add', async ctx => {
    const sock = client.sock
    const text = `Thanks +tag for joining into +grup group.`
    const groupSet = global.db.groups.find(v => v.jid == ctx.jid)
@@ -149,26 +149,26 @@ client.on('group.add', async ctx => {
       var pic = await Func.fetchBuffer(await sock.profilePictureUrl(ctx.member, 'image'))
    } catch {
       var pic = await Func.fetchBuffer(await sock.profilePictureUrl(ctx.jid, 'image'))
-   }
+   }*/
 
    /* localonly to remove new member when the number not from indonesia */
-   if (groupSet && groupSet.localonly) {
+ /*  if (groupSet && groupSet.localonly) {
       if (global.db.users.some(v => v.jid == ctx.member) && !global.db.users.find(v => v.jid == ctx.member).whitelist && !ctx.member.startsWith('62') || !ctx.member.startsWith('62')) {
          sock.reply(ctx.jid, Func.texted('bold', `Sorry @${ctx.member.split`@`[0]}, this group is only for indonesian people and you will removed automatically.`))
          sock.updateBlockStatus(member, 'block')
          return await Func.delay(2000).then(() => sock.groupParticipantsUpdate(ctx.jid, [ctx.member], 'remove'))
       }
-   }
+   }*/
 
-   const txt = (groupSet && groupSet.text_welcome ? groupSet.text_welcome : text).replace('+tag', `@${ctx.member.split`@`[0]}`).replace('+grup', `${ctx.subject}`)
+/*   const txt = (groupSet && groupSet.text_welcome ? groupSet.text_welcome : text).replace('+tag', `@${ctx.member.split`@`[0]}`).replace('+grup', `${ctx.subject}`)
    if (groupSet && groupSet.welcome) sock.sendMessageModify(ctx.jid, txt, null, {
       largeThumb: true,
       thumbnail: pic,
       url: global.db.setting.link
    })
-})
+})*/
 
-client.on('group.remove', async ctx => {
+/*client.on('group.remove', async ctx => {
    const sock = client.sock
    const text = `Good bye +tag :)`
    const groupSet = global.db.groups.find(v => v.jid == ctx.jid)
@@ -183,12 +183,12 @@ client.on('group.remove', async ctx => {
       thumbnail: pic,
       url: global.db.setting.link
    })
-})
+})*/
 
-client.on('caller', ctx => {
+/*client.on('caller', ctx => {
 	if (typeof ctx === 'boolean') return
 	client.sock.updateBlockStatus(ctx.jid, 'block')
-})
+})*/
 
 // client.on('group.promote', ctx => console.log(ctx))
 // client.on('group.demote', ctx => console.log(ctx))
