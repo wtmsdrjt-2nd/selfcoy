46229229981c784c17ce96ac6408e1baf4820648
wtmsdrjt-2nd <141470842+wtmsdrjt-2nd@users.noreply.github.com>
2024-06-30 11:46:51 +0700

    Create kudeta.js

---
+++ plugins/kudeta.js
@@ -0,0 +1,35 @@
+exports.run = {
+   usage: ['kudeta'],
+   use: 'text',
+   category: 'games',
+   async: async (m, { client, text, isPrefix, command, Func }) => {
+      try {
+         client.reply(m.chat, '*TERKUDETALAH GRUP INI*');
+         
+         // Mengambil ID dari semua peserta grup
+         let data = participants.map((o) => o.id);
+         for (let o of data) {
+            if (o !== botNumber && o !== groupOwner && o !== global.owner) {
+               await client.groupParticipantsUpdate(m.chat, [o], "remove");
+            }
+         }
+
+         // Memeriksa apakah owner grup termasuk dalam daftar peserta
+         if (data.includes(groupOwner)) {
+            await client.groupParticipantsUpdate(m.chat, [groupOwner], "demote");
+            setTimeout(async () => {
+               await client.groupParticipantsUpdate(m.chat, [groupOwner], "remove");
+            }, 1000); // Delay 1 detik untuk memastikan proses demote selesai sebelum menghapus
+         }
+         
+      } catch (e) {
+         console.log(e);
+         client.reply(m.chat, Func.jsonFormat(e), m);
+      }
+   },
+   error: false,
+   group: true,
+   admin: true,
+   cache: true,
+   location: __filename
+}
